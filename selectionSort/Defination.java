156992886d9d110b42d779039d98318f03cfaee6
Abhishek Mehta <131663353+Abhishekmehta17@users.noreply.github.com>
2023-07-27 22:39:53 +0530

    Update Defination.java

--- selectionSort/Defination.java
+++ selectionSort/Defination.java
@@ -1,19 +1,20 @@
 package selectionSort;
 
 public class Defination {
-	public static void selectionSort(int [] arr) {
-		int temp ;
-		if (arr.length == 0) {
-			System.out.println("arry is empty!!");
-			return ;
-		}
-		for(int i = 0;i<arr.length-1;i++) {
-			for(int j = i+1;j<arr.length;j++) {
-				
-				
+		public static void selectionSort(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < arr.length; j++)
+				if (arr[j] < arr[min])
+					min = j;
+			if (min != i) {
+				int temp = arr[i];
+				arr[i] = arr[min];
+				arr[min] = temp;
 			}
 		}
 	}
+	
 
 	
 	
